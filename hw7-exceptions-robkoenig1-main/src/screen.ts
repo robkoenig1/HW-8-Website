eb75a494ec9b0c32f830fd92e67d0091b71fd542
Robert Koenig <rkoenig@udel.edu>
2024-10-17 16:36:26 -0400

    HW-7 rev9

--- hw7-exceptions-robkoenig1-main/src/screen.ts
+++ hw7-exceptions-robkoenig1-main/src/screen.ts
@@ -29,7 +29,7 @@ export class Screen {
     constructor(pixels: Pixel[][]) {
         let lenght: number = pixels[0].length;
         for (let i: number = 0; i < pixels.length; i++) {
-            if (pixels[i].length !== lenght) {
+            if (pixels[i].length != lenght) {
                 throw new Error("Input array is not rectangular");
             }
         }
